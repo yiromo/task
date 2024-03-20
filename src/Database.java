ecb5932eba9e5b777d4797f33bb9febed9322478
yi <yungpxxp@gmail.com>
2024-03-20 17:25:46 +0600

    creating database

---
+++ src/Database.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+
+public class Database {
+
+    static String Database_url = "jdbc:postgresql://localhost:5432/taskDB";
+
+    static String Login = "postgres";
+    static String Pass = "13012005aA";
+
+}
