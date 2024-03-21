7cc105a92968347fed5b1b3aec9a03810798c245
yi <yungpxxp@gmail.com>
2024-03-21 17:03:30 +0600

    Commit

--- src/Database.java
+++ src/Database.java
@@ -2,18 +2,17 @@ import java.sql.*;
 
 public class Database {
 
-    static String Database_url = "jdbc:postgresql://localhost:5432/taskDB";
 
+    static String Database_url = "jdbc:postgresql://localhost:5432/taskdb";
     static String Login = "postgres";
     static String Pass = "13012005aA";
-
     public static int wordCountDb(String text) {
         return text.split("\\s+").length;
     }
     public static void main(String[] args){
         try(Connection connection = DriverManager.getConnection(Database_url, Login, Pass);
             Statement statement = connection.createStatement()){
-            String sql = "select dbtext from taskDB where length(dbtext) - length(replace(dbtext, ' ', '')) + 1 >= 5;" ;
+            String sql = "select dbtext from task1_table where length(dbtext) - length(replace(dbtext, ' ', '')) + 1 >= 5;" ;
             try (ResultSet resultSet = statement.executeQuery(sql)){
                 while (resultSet.next()){
                     String full_text = resultSet.getString("dbtext");
